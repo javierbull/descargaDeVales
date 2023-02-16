7293a43acf159d0b89ea21eafe5e3135339313a5
javierbull <javierbullreyes@gmail.com>
2023-02-16 17:11:35 -0500

    vale general

--- js/main.js
+++ js/main.js
@@ -4,12 +4,20 @@ $(document).ready(function(){
 
   $("#showValesCineplanet").click(function() {
     $("#section-cards").hide();
-    $("#section-vales").show();
+    $("#section-vales-cineplanet").hide();
+    $("#section-vales-coney").show();
+  });
+
+  $("#showValesDonofrio").click(function() {
+    $("#section-cards").hide();
+    $("#section-vales-cineplanet").hide();
+    $("#section-vales-coney").show();
   });
 
   $(".showCardsCompra").click(function() {
     $("#section-cards").show();
-    $("#section-vales").hide();
+    $("#section-vales-cineplanet").hide();
+    $("#section-vales-coney").hide();
   });
 
 });
